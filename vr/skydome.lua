4e55bcb532aca95a4800831dad6f61e993fb7d99
Juan Sebastian Casallas <casallas@iastate.edu>
2012-04-16 16:56:56 -0500

    Add skydome script
    Loads the skydome model, correcting its pose

---
+++ vr/skydome.lua
@@ -0,0 +1,20 @@
+-- Look for models in the same directory as this file.
+require("getScriptFilename")
+fn = getScriptFilename()
+assert(fn, "Have to load this from file, not copy and paste, or we can't find our models!")
+vrjLua.appendToModelSearchPath(fn)
+
+-- Skydome model transform, we need this to rotate -90 on x, and shift it up 2m
+skydomeXform = Transform{
+	position = {0, 2, 0},
+	-- set the transform orientation to -90 on the x (first element of Axis values)
+	orientation = AngleAxis(Degrees(-90), Axis{1.0, 0.0, 0.0}),
+}
+-- Load the skydome model
+local m = Model("model/skydome11.osg")
+
+-- Add the model to the transform
+skydomeXform:addChild(m)
+
+-- Add the transform to the world
+RelativeTo.World:addChild(skydomeXform)
