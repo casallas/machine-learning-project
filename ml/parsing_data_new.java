8ae9961c4a2dbf07406f7e67af11a91285324c80
Juan Sebastian Casallas <casallas@iastate.edu>
2012-04-25 17:03:02 -0500

    Ashwin: detect trial endings more accurately

--- ml/parsing_data_new.java
+++ ml/parsing_data_new.java
@@ -19,6 +19,8 @@ public class parsing_data_new {
 	private String ballPositionsandSizeData;
 	private ArrayList<String> positions;
 	private FileWriter fw;
+	private int lineNumber = 0;
+	private int numberOfTrials = 0;
 
 	public parsing_data_new(int numballs, FileWriter fw) {
 		// this.toParse = line;
@@ -42,12 +44,17 @@ public class parsing_data_new {
 	}
 
 	public void scan(String toParse) {
+		lineNumber++;
+		if (toParse.startsWith("<new_trial/>"))
+			numberOfTrials++;
 		if (toParse.startsWith("<new_trial/>") && trials == false) {
 			trials = true;
-		} else if (toParse.startsWith("<new_trial/>") && trials == true) {
+		} else if ((toParse.startsWith("<no_more_spheres/>") || toParse.startsWith("<balls_bypassed_user/>") ) && trials == true) {
 			// trials = false;
 			try {
+				//System.out.println(lineNumber);
 				fw.write(this.toString());
+				//System.out.println("Written output for" + numberOfTrials);
 				firstInstanceinTrial = false;
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -75,7 +82,7 @@ public class parsing_data_new {
 					String ballPositions = features[6 + ballsnum].split("_")[2];
 				    ballPositionsandSizeData += ballSize + "," + ballPositions.split(" ")[0] + "," + ballPositions.split(" ")[1] + "," + ballPositions.split(" ")[2];
 				    if(ballsnum < numballs - 1)
-						ballPositionsandSizeData += ",";
+					ballPositionsandSizeData += ",";
 				}
 				}
 				positions.add(requiredFeatures);
@@ -87,6 +94,10 @@ public class parsing_data_new {
 		}
 	}
 
+	public int getNumberOfTrials(){
+		return numberOfTrials;
+	}
+
 	public static void main(String[] args) {
 		// check for
 		if (args.length != 3) {
@@ -95,26 +106,34 @@ public class parsing_data_new {
 		}
 		// create file scanner for input file
 		File fin = new File(args[0]);
-
+        int numberOfBalls = Integer.parseInt(args[1]);
 		File fout = new File(args[2]);
 		FileWriter fw = null;
 		try {
 			fw = new FileWriter(fout);
+			String ballsHeader = "";
+			if(numberOfBalls == 3)
+				ballsHeader = "ball1_size,ball1_0_x, ball1_0_y, ball1_0_z, ball2_size, ball2_0_x, ball2_0_y, ball2_0_z," +
+						"ball3_size, ball3_0_x, ball3_0_y, ball3_0_z, collision1, collision2, collision3";
+			if(numberOfBalls == 2)
+				ballsHeader = "ball1_size,ball1_0_x, ball1_0_y, ball1_0_z, ball2_size, ball2_0_x, ball2_0_y, ball2_0_z," +
+						"collision1, collision2";
+			if(numberOfBalls == 1)
+				ballsHeader = "ball1_size,ball1_0_x, ball1_0_y, ball1_0,z," +
+						 "collision1";
 			fw.write("total_time,head_position_0_x,head_position_0_y,head_position_0_z," +
 					"head_orientation_0_x,head_orientation_0_y,head_orientation_0_z,head_orientation_0_w,"  +
 					"wand_position_0_x,wand_position_0_y,wand_position_0_z," +
-					"wand_orientation_0_x,wand_orientation_0_y,wand_orientation_0_z,wand_orientation_0_w," + 
+					"wand_orientation_0_x,wand_orientation_0_y,wand_orientation_0_z,wand_orientation_0_w," +
 					"head_position_i3_x,head_position_i3_y,head_position_i3_z," +
-					"head_orientation_i3_x,head_orientation_i3_y,head_orientation_i3_z,head_orientation_i3_w," + 
+					"head_orientation_i3_x,head_orientation_i3_y,head_orientation_i3_z,head_orientation_i3_w," +
 					"wand_position_i3_x,wand_position_i3_y,wand_position_i3_z," +
 					"wand_orientation_i3_x,wand_orientation_i3_y,wand_orientation_i3_z,wand_orientation_i3_w," +
 					"head_position_i3_x,head_position_i3_y,head_position_i3_z," +
 					"head_orientation_i3_x,head_orientation_i3_y,head_orientation_i3_z,head_orientation_i3_w," +
 					"wand_position_i3_x,wand_position_i3_y,wand_position_i3_z," +
 					"wand_orientation_i3_x,wand_orientation_i3_y,wand_orientation_i3_z,wand_orientation_i3_w," +
-					"numballs, ball1_size,ball1_0_x, ball1_0_y, ball1_0,z, ball2_size, ball2_0_x, ball2_0_y, ball2_0_z," +
-					"ball3_size, ball3_0_x, ball3_0_y, ball3_0_z, collision1, collision2, collision3\n"
-					);
+					"numballs," + ballsHeader + "\n");
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -133,6 +152,15 @@ public class parsing_data_new {
 		while (scan.hasNextLine()) {
 			line = scan.nextLine();
 			pd.scan(line);
+		}
+
+		try{
+		//fw.write(pd.toString());
+			fw.close();
+			//System.out.println(pd.getNumberOfTrials());
+		}catch (IOException e){
+			System.err.println("IO Exception" + args[0]);
+			return;
 		}
 	}
 
